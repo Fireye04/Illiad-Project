2005ad5c37f8dff857b111727b7e2b7f5ad9300d
Fireye <codekai16@gmail.com>
2022-11-29 13:23:00 -0700

    dev 2

--- main.java
+++ main.java
@@ -3,8 +3,13 @@ public class Main {
 
     public static void main(String[] args) {
         
-        System.out.println("\nDear Fabled hero, \n you have been summoned by his majesty Agamemnon of Argos. You are required to set sail for Troy with great haste or we will set Corinth to flames.\n");
         
-            
+
+        System.out.println("\nDear Fabled hero, \nyou have been summoned by his majesty Agamemnon of Argos. You are required to set sail for Troy with great haste or we will set Corinth to flames.\n");
+        System.out.println("Do you accept? ");
+
+        System.out.println("You have chosen not to set sail, your wife and her lover from Lesbos have killed you with arsenic in your soup. You are dead.");
+        
+        
     }
 }
