0625cd890c12c2a504a70c2657fecc12dd35487e
Fireye <codekai16@gmail.com>
2022-12-01 12:06:56 -0700

    update

--- main.java
+++ main.java
@@ -5,13 +5,11 @@ public class Main {
         
         Scanner in = new Scanner(System.in);
 
-        boolean inloop = true;
-        
         System.out.println("\nDear Fabled hero, \nyou have been summoned by his majesty Agamemnon of Argos. \nYou are required to set sail for Troy with great haste or we will set Corinth to flames.\n");
         
         int cNum = 0;
         String choice = "";
-
+        boolean inloop = true;
         while (inloop) {
 
             System.out.println("Do you accept? (1 for yes, 2 for no) ");
@@ -42,7 +40,9 @@ public class Main {
         
         System.out.println("Before you set sail you chose to recruit a first officer for your fleet. \n Select a first officer. \n1) Ophelia, Daughter of Athena, Great hero of Athens \n2) Atlas, Son Of Poseidon, Great saviour of Myceaene \n3) Daryl, Some Guy");
         
-        cNum = 0;
+        int cNum = 0;
+        String choice = "";
+        boolean inloop = true;
 
         while (inloop) {
 
@@ -58,14 +58,19 @@ public class Main {
                     System.out.println("bad numberz");
                 }
                 
-             }
-             catch (NumberFormatException e) {
+            } catch (NumberFormatException e) {
                 System.out.println("that's a word. words are not numbers.");
-             }
-            System.out.println("Go to the shop?");
-            System.out.println("Rope \nEarwax \nMagic Mirror \n Wood");
+            }
 
         }
+
+        System.out.println("Go to the shop?");
+        System.out.println("Rope \nEarwax \nMagic Mirror \n Wood");
+
+    }
+    //haha penis so gay haha
+    public static int inputLoop (String prompt, int options) {
+
     }
 
     public static void dead () {
